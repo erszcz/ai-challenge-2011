4ffc123f57122fae4f7f690cab3777b4cfd50b3e
Radosław Szymczyszyn <lavrin@gmail.com>
2011-10-29 21:16:05 +0200

    WIP: major change for better local decision making
    Ants will be able to make decision based on number of inputs from their
    closest environment: number of enemies, allies, distances to them, etc.

--- MyBot.py
+++ MyBot.py
@@ -24,41 +24,20 @@ def v2add(a, b):
 def v2sub(a, b):
     return a[0] - b[0], a[1] - b[1]
 
-def get_path(start, goal, ants):
-    #log.debug("get_path: g =\n  %s" % str(g))
-
-    graph_h = ants.rows
-    graph_w = ants.cols
-    rmin, rmax = [f(start[0], goal[0]) for f in [min, max]]
-    cmin, cmax = [f(start[1], goal[1]) for f in [min, max]]
-
-    def adjacent(node):
-        #log.debug("adjacent: %s" % str(node))
-        r, c = node
-        in_graph = lambda p: p[0] >= rmin and p[0] <= rmax \
-                and p[1] >= cmin and p[1] <= cmax
-        return filter(lambda p: in_graph(p) and ants.passable(p),
-                [ (r, c-1), (r, c+1), (r-1, c), (r+1, c) ])
-
-    p = astar.path(ants.map, start, goal, adjacent, ants.distance,
-            astar.h_cross)
-    
-    #log.debug("get_path: path =\n  %s" % str(p))
-    #dump_path("path_%s_%s_%s.dat" % (start, goal, time.time()),
-            #graph, start, goal, p, passable=passable)
-    return p
-
 class MyBot:
     def __init__(self):
         self.ants_straight = {}
         self.ants_lefty = {}
         self.ants_tracking = {}
         self.ants_guarding = {}
+        self.ants_adhoc = set()
         self.leaving = set()
         self.turns = 0
-        self.food = []
-        self.enemy_hills = []
-        self.my_hills = []
+        self.enemy_hills = set()
+        self.enemy_ants  = set()
+        self.food        = set()
+        self.my_ants     = set()
+        self.my_hills    = set()
         self.ants = None
         self.guard_threshold = 0.0
 
@@ -71,12 +50,19 @@ class MyBot:
         pass
 
         self.viewradius = int(sqrt(ants.viewradius2)) + 1
+        self.attackradius = int(sqrt(ants.attackradius2)) + 1
 
     def start_turn(self, ants):
         self.ants = ants
-        self.food = ants.food()
-        self.enemy_hills = ants.enemy_hills()
-        self.my_hills = ants.my_hills()
+        self.enemy_hills = set( ( loc
+                for loc, owner in ants.hill_list.items()
+                if owner != MY_ANT) )
+        self.enemy_ants  = set( ( (row, col)
+                for (row, col), owner in ants.ant_list.items()
+                if owner != MY_ANT ) )
+        self.food        = set(ants.food())
+        self.my_ants     = set(ants.my_ants())
+        self.my_hills    = set(ants.my_hills())
         
         self.destinations = []
         self.new_straight = {}
@@ -95,44 +81,123 @@ class MyBot:
         self.ants_lefty = self.new_lefty
         self.ants_tracking = self.new_tracking
         self.ants_guarding = self.new_guarding
+        self.ants_adhoc.clear()
+
+    def scan(self, ant):
+        ehills, enemies, food, allies, hills = [], [], [], [], []
+        for loc in self.field_of_view(ant):
+            if loc in self.enemy_hills:
+                ehills.append(loc)
+            elif loc in self.enemy_ants:
+                enemies.append(loc)
+            elif loc in self.food:
+                food.append(loc)
+            elif loc in self.my_ants and loc != ant:
+                allies.append(loc)
+            elif loc in self.my_hills:
+                hills.append(loc)
+        distance_to = lambda target: self.ants.distance(ant, target)
+        for list in (ehills, enemies, food, allies, hills):
+            list.sort(key=distance_to)
+        result = ehills, enemies, food, allies, hills
+        log.debug("  scan: %s" % str(result))
+        return ehills, enemies, food, allies, hills
 
     def do_turn(self, ants):
         self.start_turn(ants)
 
         for ant in ants.my_ants():
+            distance_to = lambda target: ants.distance(ant, target)
             a_row, a_col = ant
             log.info("turn %d: ant %s" % (self.turns, ant))
             log.debug("  time remaining: %d" % ants.time_remaining())
 
-            # look for targets
-            if not ant in self.ants_tracking \
-                    and (self.enemy_hills or self.food):
-                path = None
-                log.debug("  looking for targets")
-                targets = []
-                for loc in self.field_of_view(ant):
-                    if loc in self.enemy_hills \
-                            or loc in self.food:
-                        targets.append(loc)
-                distance_to = lambda target: ants.distance(ant, target)
-                targets.sort(key=distance_to)
-                for loc in targets:
-                    path = get_path(ant, loc, ants)
-                    log.info("  found %s target: %s" %
-                        ("reachable" if path else "unreachable", loc))
+            # scan for what's in the vicinity
+            ehills, enemies, food, allies, hills = self.scan(ant)
+
+            # look for dangers
+            # - hill is to be razed? defend
+            if hills and enemies:
+                log.debug("  defending")
+                if ant == hills[0]:
+                    log.debug("    blocking the hill")
+                    self.cancel_action(ant)
+                else:
+                    path = self.get_path(ant, hills[0], unoccupied=True)
                     if path:
-                        log.info("  starts tracking target")
-                        self.new_tracking[ant] = path[1:]
-                        self.ants_tracking[ant] = path
-                        break # path found
-                if path:
-                    continue # to next ant
+                        log.debug("    found path to hill")
+                        self.cancel_action(ant)
+                        self.ants_tracking[ant] = path[1:]
+                    else:
+                        log.debug("    can't reach hill")
+            # - too much enemies to cope with? flee
+            elif enemies:
+                log.debug("  enemies (%s) in view" % len(enemies))
+                close_allies = filter( \
+                    lambda a: distance_to(a) < self.attackradius, allies)
+                close_enemies = filter( \
+                    lambda a: distance_to(a) < self.attackradius, enemies)
+                log.debug("    close enemies: %s" % close_enemies)
+                log.debug("    close allies: %s" % close_allies)
+                if len(close_enemies) >= len(close_allies) and \
+                        distance_to(enemies[0]) < self.attackradius:
+                    # let's try to flee
+                    log.debug("  fleeing")
+                    directions = ants.direction(enemies[0], ant)
+                    done = False
+                    for d in directions:
+                        dest = ants.destination(ant, d)
+                        if self.move(ant, dest, d):
+                            done = True
+                            self.cancel_action(ant)
+                            self.ants_adhoc.add(ant)
+                            break
+                    # can't flee? try to step on enemy ant so both die
+                    if not done:
+                        log.debug("  can't flee")
+                        directions = ants.direction(ant, enemies[0])
+                        for d in directions:
+                            dest = ants.destination(ant, d)
+                            if dest == enemies[0]:
+                                if self.move(ant, dest, d):
+                                    self.cancel_action(ant)
+                                    self.ants_adhoc.add(ant)
+                                break
+                else:
+                    close_allies = filter( \
+                        lambda a: distance_to(a) < self.attackradius, allies)
+                    if close_allies:
+                        log.debug("  attacking")
+                        done = False
+                        directions = ants.direction(ant, enemies[0])
+                        for d in directions:
+                            dest = ants.destination(ant, d)
+                            if self.move(ant, dest, d):
+                                done = True
+                                self.cancel_action(ant)
+                                self.ants_adhoc.add(ant)
+                                break
+            elif ehills:
+                log.debug("  razing")
+                path = None
+                while not path and ehills:
+                    self.cancel_action(ant)
+                    path = self.track(ant, ehills[0], "enemy hill")
+                    ehills = ehills[1:]
+            elif food:
+                log.debug("  harvesting")
+                path = None
+                while not path and food:
+                    self.cancel_action(ant)
+                    path = self.track(ant, food[0], "food")
+                    food = food[1:]
 
             # new (or free) ants:
             if (not ant in self.ants_straight and
                     not ant in self.ants_lefty and
                     not ant in self.ants_tracking and
-                    not ant in self.ants_guarding):
+                    not ant in self.ants_guarding and
+                    ant not in self.ants_adhoc):
                 distance_to = lambda target: ants.distance(ant, target)
                 choice = random.random()
                 if choice < self.guard_threshold:
@@ -165,6 +230,35 @@ class MyBot:
                 if not done and not ant in self.destinations:
                     self.new_guarding[ant] = hill
                     self.destinations.append(ant)
+
+            # tracking a path?
+            if ant in self.ants_tracking:
+                log.info("  is tracking")
+                path = self.ants_tracking[ant]
+                dest, path_tail = path[0], path[1:]
+                log.debug("  dest: %s, tail: %s", dest, path_tail)
+                if ants.passable(dest):
+                    log.debug("  dest is passable")
+                    if self.unoccupied(dest) and not dest in self.destinations:
+                        direction = ants.direction(ant, dest)[0]
+                        ants.issue_order((ant, direction))
+                        self.leaving.add(ant)
+                        self.destinations.append(dest)
+                        # path unfinished?
+                        if path_tail:
+                            self.new_tracking[dest] = path_tail
+                        # that was the last step, do something else
+                        else:
+                            self.new_straight[ant] = direction
+                    else:
+                        log.debug("  dest is occupied or in destinations")
+                        path.insert(0, ant)
+                        shuffle(DIRECTIONS)
+                        for d in DIRECTIONS:
+                            dest = ants.destination(ant, d)
+                            if self.move(ant, dest):
+                                self.new_tracking[dest] = path
+                                break
 
             # send ants going in a straight line in the same direction
             if ant in self.ants_straight:
@@ -212,35 +306,6 @@ class MyBot:
                                 self.destinations.append(ant)
                                 break
 
-            # tracking a path?
-            if ant in self.ants_tracking:
-                log.info("  is harvesting")
-                path = self.ants_tracking[ant]
-                dest, path_tail = path[0], path[1:]
-                log.debug("  dest: %s, tail: %s", dest, path_tail)
-                if ants.passable(dest):
-                    log.debug("  dest is passable")
-                    if self.unoccupied(dest) and not dest in self.destinations:
-                        direction = ants.direction(ant, dest)[0]
-                        ants.issue_order((ant, direction))
-                        self.leaving.add(ant)
-                        self.destinations.append(dest)
-                        # path unfinished?
-                        if path_tail:
-                            self.new_tracking[dest] = path_tail
-                        # that was the last step, do something else
-                        else:
-                            self.new_straight[ant] = direction
-                    else:
-                        log.debug("  dest is occupied or in destinations")
-                        path.insert(0, ant)
-                        shuffle(DIRECTIONS)
-                        for d in DIRECTIONS:
-                            dest = ants.destination(ant, d)
-                            if self.move(ant, dest):
-                                self.new_tracking[dest] = path
-                                break
-
         self.end_turn()
 
     def unoccupied(self, loc):
@@ -249,8 +314,9 @@ class MyBot:
             return True
         return self.ants.unoccupied(loc)
 
-    def move(self, ant, dest, direction=None):
-        if self.unoccupied(dest) and not dest in self.destinations:
+    def move(self, ant, dest, direction=None, skip_unoccupied=False):
+        if (skip_unoccupied or self.unoccupied(dest)) \
+                and not dest in self.destinations:
             d = direction if direction else self.ants.direction(ant, dest)[0]
             self.ants.issue_order((ant, d))
             self.leaving.add(ant)
@@ -261,12 +327,55 @@ class MyBot:
     def field_of_view(self, ant):
         vr = self.viewradius
         ar, ac = ant
-        for r in xrange(ar - vr, ar):
-            for c in xrange(ac - vr, ac):
+        for r in xrange(ar - vr, ar + vr + 1):
+            for c in xrange(ac - vr, ac + vr + 1):
                 yield (r,c)
-        for r in xrange(ar + 1, ar + vr + 1):
-            for c in xrange(ac + 1, ac + vr + 1):
-                yield (r,c)
+
+    def get_path(self, start, goal, unoccupied=False):
+        #log.debug("get_path: g =\n  %s" % str(g))
+
+        graph_h = self.ants.rows
+        graph_w = self.ants.cols
+        rmin, rmax = [f(start[0], goal[0]) for f in [min, max]]
+        cmin, cmax = [f(start[1], goal[1]) for f in [min, max]]
+
+        in_graph = lambda p: p[0] >= rmin and p[0] <= rmax \
+            and p[1] >= cmin and p[1] <= cmax
+
+        passable = self.ants.passable if not unoccupied else \
+            lambda p: self.ants.passable(p) and self.ants.unoccupied(p)
+
+        def adjacent(node):
+            #log.debug("adjacent: %s" % str(node))
+            r, c = node
+            return filter(lambda p: in_graph(p) and passable(p),
+                    [ (r, c-1), (r, c+1), (r-1, c), (r+1, c) ])
+
+        p = astar.path(self.ants.map, start, goal, adjacent, self.ants.distance,
+                astar.h_cross)
+        
+        #log.debug("get_path: path =\n  %s" % str(p))
+        #dump_path("path_%s_%s_%s.dat" % (start, goal, time.time()),
+                #graph, start, goal, p, passable=passable)
+        return p
+
+    def track(self, ant, target, name="target"):
+        path = self.get_path(ant, target)
+        if path:
+            log.debug("    found path to %s" % name)
+            self.ants_tracking[ant] = path[1:]
+        else:
+            log.debug("    can't reach %s" % name)
+        return path
+
+    def cancel_action(self, ant):
+        dicts = [ self.ants_straight, self.ants_lefty, self.ants_tracking,
+                  self.ants_guarding ]
+        for d in dicts:
+            if ant in d:
+                d.pop(ant)
+        if ant in self.ants_adhoc:
+            self.ants_adhoc.remove(ant)
 
 if __name__ == '__main__':
     try:
